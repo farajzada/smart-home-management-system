b1088711682ab884d995db49fb625d2f8abdb408
farajzada <mfarajzadaa@gmail.com>
2025-01-03 23:02:51 +0400

    Device.java attached

---
+++ src/Device.java
@@ -0,0 +1,36 @@
+public class Device {
+    public enum DeviceType {
+        LIGHT,
+        THERMOSTAT,
+        CAMERA,
+        DOORBELL
+    }
+
+    private int id;
+    private String name;
+    private boolean status;
+    private DeviceType deviceType;
+
+
+    public Device(int id, String name, boolean status, DeviceType deviceType) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+        this.deviceType = deviceType;
+    }
+
+    public void turnOn(){};
+    public void turnOff(){};
+
+    public boolean getStatus(){
+        return status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
