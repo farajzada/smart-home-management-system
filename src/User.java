fffdca10f6d4e43a3f317007218a4039cab33f97
farajzada <mfarajzadaa@gmail.com>
2025-01-03 23:02:28 +0400

    User.java attached

---
+++ src/User.java
@@ -0,0 +1,25 @@
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void register() {};
+    public void login() {};
+    public void logout() {};
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
